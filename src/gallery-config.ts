a2114e2958cf3a23215df62ea63e81778a905b0f
Surendra Rathod <rathoresurendra80@gmail.com>
2026-04-08 17:16:44 -0400

    Add gallery, update copyright, remove Home/Latest nav

---
+++ src/gallery-config.ts
@@ -0,0 +1,39 @@
+// Gallery Configuration
+// Add your trips and photos here - it's that easy!
+
+export const GALLERY_TRIPS = [
+  {
+    id: 'lake-placid-2024',
+    title: 'Lake Placid, NY',
+    date: 'October 2024',
+    description: 'Fall colors and mountain views in the Adirondacks',
+    coverImage: '/images/gallery/lake-placid/cover.jpg',
+    photos: [
+      {
+        src: '/images/gallery/lake-placid/photo1.jpg',
+        caption: 'Mirror Lake at sunset',
+        alt: 'Lake Placid mirror lake'
+      },
+      {
+        src: '/images/gallery/lake-placid/photo2.jpg',
+        caption: 'Fall foliage trail',
+        alt: 'Adirondack fall colors'
+      },
+      {
+        src: '/images/gallery/lake-placid/photo3.jpg',
+        caption: 'Mountain peaks',
+        alt: 'Mountain view'
+      },
+      // Add more photos here...
+    ]
+  },
+  // Add more trips below:
+  // {
+  //   id: 'grand-canyon-2023',
+  //   title: 'Grand Canyon',
+  //   date: 'Summer 2023',
+  //   description: 'Exploring the vast beauty of the Grand Canyon',
+  //   coverImage: '/images/gallery/grand-canyon/cover.jpg',
+  //   photos: [...]
+  // },
+];
